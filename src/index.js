663e58f246bb27ad800cae44ba6b9315c703c1c1
Anna <mambarinka@mail.ru>
2021-02-04 16:11:40 +0400

    feat: implement 3 function

--- src/index.js
+++ src/index.js
@@ -1,12 +1,48 @@
+exports.min = function min(array) {
+    let minNumber;
+    if (!array || array.length === 0) {
+        minNumber = 0;
+    } else {
+        minNumber = array.reduce((calcNumber, currentNumber) => {
+            return currentNumber < calcNumber ? currentNumber : calcNumber;
+        });
+    }
+    return minNumber;
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+// 2 сокращенный вариант с минимальным числом
 
-exports.max = function max (array) {
-  return 0;
-}
+// exports.min = function min(array) {
+//     if (!array || array.length === 0) {
+//         return 0;
+//     } else {
+//         return Math.min(...array);
+//     }
+// };
 
-exports.avg = function avg (array) {
-  return 0;
-}
+
+exports.max = function max(array) {
+    let maxNumber;
+    if (!array || array.length === 0) {
+        maxNumber = 0;
+    } else {
+        maxNumber = array.reduce((calcNumber, currentNumber) => {
+            return currentNumber > calcNumber ? currentNumber : calcNumber;
+        });
+    }
+    return maxNumber;
+};
+
+exports.avg = function avg(array) {
+    let averageNumber;
+    if (!array || array.length === 0) {
+        averageNumber = 0;
+    } else {
+        averageNumber = array.reduce((calcNumber, currentNumber, index, array) => {
+            return (currentNumber + calcNumber);
+        });
+        averageNumber = averageNumber / array.length;
+    }
+
+    return averageNumber;
+};
